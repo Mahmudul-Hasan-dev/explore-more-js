dc262e825fa4c53499a6ebae266759539ba3026b
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-10 17:19:00 +0600

    comment added

--- recursive.js
+++ recursive.js
@@ -2,7 +2,7 @@ function sum(num) {
     if (num == 1) {
         return 1;
     }
-    return num + sum(num - 1);
+    return num + sum(num - 1);//here function is called internally until it call off by condition 
 }
 const total = sum(6);
 console.log(total);
