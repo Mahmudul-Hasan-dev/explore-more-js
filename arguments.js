730562b428e180c7c6eba0ec62454af141f67c85
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-10 19:15:10 +0600

    arguments done

---
+++ arguments.js
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+    //arguments show all the parameter with index that have been sent when calling function
+    //arguments is only available inside the function
+    // arguments is array like object
+    console.log(arguments)
+}
+add(10, 12);
