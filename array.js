9400169baaee2ebdfd6058377294bf6652165527
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-10 19:04:28 +0600

    array like object done

---
+++ array.js
@@ -0,0 +1,22 @@
+const products = [
+    { name: 'phone', price: 12000 },
+    { name: 'lenovo', price: 13000 },
+    { name: 'apple', price: 130000 },
+    { name: 'oppo', price: 23000 }
+]
+console.log(products[0]);
+console.log(products[1]);
+
+//array like object describe
+
+const product = [10, 12, 13, 14];
+
+//array like object
+//not array but like array
+
+const product2 = {
+    '0': 10,
+    '1': 12,
+    '2': 13,
+    '3': 14
+}
