09ac068e7eef1da21717c6e42a85721a641a1a9a
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-11 08:19:15 +0600

    for of done

---
+++ matched-for-of.js
@@ -0,0 +1,8 @@
+const numbers = [12, 14, 56, 675, 32, 34];
+
+//for of use 
+//we can access every element using for of  loop
+//structure :: for(variable of arrayName)
+for (const number of numbers) {
+    console.log(number);
+}
---
+++ matched.js
@@ -0,0 +1,6 @@
+const numbers = [12, 13, 14, 23, 24, 35, 32];
+
+for (i = 0; i < +numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
+}
